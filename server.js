8a99c5d4bf30b82265111bdeb790c3303406cf11
James <james88@acanac.net>
2018-06-10 11:34:20 -0400

    fix connection

--- server.js
+++ server.js
@@ -11,13 +11,17 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    port : '5000',
-    user : 'postgres',
-    password : 'postgres',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
   }
-});
+  // {
+    // host : '127.0.0.1',
+    // port : '5000',
+    // user : 'postgres',
+    // password : 'postgres',
+    // database : 'smart-brain'
+  // }
+}); 
 
 
 const app = express();
